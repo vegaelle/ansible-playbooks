53176e84052cdf52dbc614362cc03a781b3c23d4
Damien Nicolas <damien@gordon.re>
2020-01-28 21:41:28 +0100

    [WIP] first draft of new approach

--- add_host.py
+++ add_host.py
@@ -2,41 +2,42 @@
 import sys
 import os
 import json
+import argparse
 
 import ansible_vault
 
 
 def main():
-    host_vars = json.loads(sys.argv[1])
-    vlan = host_vars['vlan']
-    ctid = host_vars['ctid']
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', '-H', action='store', dest='host')
+    parser.add_argument_group('vars', type=json.loads)
+    args = parser.parse_args()
+    ctid = args.host
+    host_vars = args.vars
+    domains = host_vars['domains'].split()
     volume_data = {
         'pool': host_vars['zfs_pool'],
-        'domains': host_vars['domains'].split(','),
-        'vlan': vlan,
+        'main_domain': domains[0],
+        'domains': domains,
         'passphrase': host_vars['zfs_volume_passphrase'],
         'backup_passphrase': host_vars['backup_passphrase'],
-        'containers': [
-            {'ctid': int(ctid),
-             'volumes': host_vars['zfs_volumes']}
-        ]
     }
 
     vault = ansible_vault.Vault(os.environ.get('ANSIBLE_VAULT_PASSWORD'))
-    with open('group_vars/all/vault.yml') as fd:
-       vault_data = vault.load(fd.read())
+    with open(f'host_vars/{ctid}/vault.yml') as fd:
+        vault_data = vault.load(fd.read())
     vault_data['zfs_volumes'][int(ctid)] = volume_data
-    if 'http' in host_vars['groups']:
-        vault_data['reverse_proxy'] = {'ctid': int(host_vars['ctid']),
-                                       'ip': f'10.0.{vlan}.{ctid}'}
-    if 'miab' in host_vars['groups']:
-        vault_data['mailinabox'] = {'ctid': int(host_vars['ctid']),
-                                    'ip': f'10.0.{vlan}.{ctid}'}
+    # if 'http' in host_vars['groups']:
+    #     vault_data['reverse_proxy'] = {'ctid': int(host_vars['ctid']),
+    #                                    'ip': f'10.0.{vlan}.{ctid}'}
+    # if 'miab' in host_vars['groups']:
+    #     vault_data['mailinabox'] = {'ctid': int(host_vars['ctid']),
+    #                                 'ip': f'10.0.{vlan}.{ctid}'}
     with open('group_vars/all/vault.yml', 'wb') as fd:
         vault.dump(vault_data, fd)
 
     with open('backups.yml') as fd:
-       vault_data = vault.load(fd.read())
+        vault_data = vault.load(fd.read())
     vault_data[int(ctid)] = host_vars['backup_passphrase']
     with open('backups.yml', 'wb') as fd:
         vault.dump(vault_data, fd)
