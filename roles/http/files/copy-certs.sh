5b9fb9544e44ea66bb3514997c25791c01c555a7
Damien Nicolas <damien@gordon.re>
2020-05-02 09:49:58 +0200

    New working approach with explicit firewall

--- roles/http/files/copy-certs.sh
+++ roles/http/files/copy-certs.sh
@@ -1,29 +1,30 @@
 #! /bin/sh
 
-nginx_dir="/etc/nginx/conf.d"
+# nginx_dir="/etc/nginx/conf.d"
 dest_certs_dir="/etc/ssl/letsencrypt"
 source_certs_dir="/home/user-data/ssl"
 
 cur_dir=`pwd`
 
-domains=()
+# domains=()
 
-cd $nginx_dir
+# cd $nginx_dir
 
-for domain in `ls 10.*`
-do
-    domains+=( ${domain:3:-5} )
-done
+# for domain in `ls 10.*`
+# do
+#     domains+=( ${domain:3:-5} )
+# done
 
 cd $source_certs_dir
 
-for domain in $domains
-do
-    domain_cert=`ls -1t $domain*.pem | head -n 1`
-    cp $source_certs_dir/$domain_cert $dest_certs_dir/$domain.pem
-done
+# for domain in $domains
+# do
+#     domain_cert=`ls -1t $domain*.pem | head -n 1`
+#     cp $source_certs_dir/$domain_cert $dest_certs_dir/$domain.pem
+# done
 
 cp ssl_private_key.pem $dest_certs_dir/
+cp ssl_certificate.pem $dest_certs_dir/
 
 cd $cur_dir
 
