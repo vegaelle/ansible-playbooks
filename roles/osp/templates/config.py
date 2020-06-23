3cba475ef5499c6c18c1fda06e90a0992b498811
Damien Nicolas <damien@gordon.re>
2020-06-23 22:27:31 +0200

    [WIP] Add OpenStreamingPlatform role

---
+++ roles/osp/templates/config.py
@@ -0,0 +1,29 @@
+# Set Database Location and Type
+# For MySQL Connections add ?charset=utf8mb4 for full Unicode Support
+dbLocation="sqlite:///db/database.db"
+
+# Redis Configuration
+redisHost="localhost" # Default localhost
+redisPort=6379 # Default 6379
+redisPassword='' # Default ''
+
+# Flask Secret Key
+secretKey="{{ osp_secret_key }}"
+
+# Password Salt Value
+passwordSalt="{{ osp_password_salt }}"
+
+# Allow Users to Register with the OSP Server
+allowRegistration=True
+
+# Require Users to Confirm their Email Addresses
+requireEmailRegistration=True
+
+# Enables Debug Mode
+debugMode = False
+
+# EJabberD Configuration
+ejabberdAdmin = "admin"
+ejabberdPass = "{{ ejabberd_pass }}"
+ejabberdHost = "localhost"
+
